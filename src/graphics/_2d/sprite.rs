cfb240de4d6a630a04cd30c064dd420a27c01152
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-07-24 19:17:37 +0200

    Update sprite.rs

--- src/graphics/_2d/sprite.rs
+++ src/graphics/_2d/sprite.rs
@@ -1,1 +1,10 @@
+use std::marker::PhantomData;
+use cgmath::Point3;
+use cgmath::prelude::*;
 
+//marker structs later
+
+struct Sprite<T> {
+    position: Point3<f32>,
+    _marker: PhantomData<T>,
+}
