26a6aff28c894fe7a1e355c5879fd50dc2d0d4fc
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-08-11 13:45:36 +0200

    Update errors.rs

--- audio/src/errors.rs
+++ audio/src/errors.rs
@@ -4,12 +4,9 @@
 
 #[derive(Debug)]
 pub enum AudioError {
-    //also temp
-    Default,
     IoError(std::io::Error),
     DecoderError(rodio::decoder::DecoderError),
     OutputStreamBuilderError(rodio::StreamError),
     StreamError(rodio::StreamError),
-    FileNotLoaded,
-    NoDevice,
+    FileNotLoaded(String),
 }
