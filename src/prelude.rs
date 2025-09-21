ed647d41137e6dec55f30959e1798a1e061d230c
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-09-21 13:15:55 +0200

    Update prelude.rs

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,1 +1,20 @@
+pub use anymap::AnyMap;
 
+pub use winit::event::WindowEvent;
+
+pub use crate::aliases::*;
+
+pub use crate::game::Game;
+
+pub use cgmath;
+
+pub use crate::world::commands::*;
+
+pub use crate::world::Entity;
+
+/***********************
+Commands
+***********************/
+
+//?
+//pub use graphics_core::camera::commands::*;
