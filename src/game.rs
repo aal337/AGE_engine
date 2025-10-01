c2c9f20ca1e0456ac715196af65169c276fbd54c
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-10-01 06:26:28 +0200

    Update game.rs

--- src/game.rs
+++ src/game.rs
@@ -1,13 +1,20 @@
 use crate::custom_events::builtin::ExitEvent;
 use crate::events::EventHandle;
 use crate::scheduler::Scheduler;
-use anymap::AnyMap;
+use anymap::{AnyMap, Map};
 use graphics_core::state::State;
 use std::convert::identity;
+use std::hint::spin_loop;
 use std::path::Path;
 
+const FPS: u32 = 60;
+
+#[derive(Debug)]
 pub struct Game {
-    pub(crate) resources: AnyMap,
+    //Terminate is a ZST (=just symbolic)
+    pub(crate) termination_tx: Option<std::sync::mpsc::SyncSender<Terminate>>,
+    pub(crate) last_iteration: Instant,
+    pub(crate) resources: SendAnyMap,
     pub(crate) scheduler: Scheduler,
     pub(crate) events: EventHandle,
     //only existent while setup
@@ -21,10 +28,12 @@ pub struct Game {
     //?
 }
 
-impl<'a> Default for Game {
+impl Default for Game {
     fn default() -> Self {
         Self {
-            resources: AnyMap::new(),
+            termination_tx: None,
+            last_iteration: Instant::now(),
+            resources: Map::new(),
             scheduler: Scheduler::new_empty(),
             events: EventHandle::new(),
             graphics_state: None,
@@ -36,6 +45,12 @@ impl<'a> Default for Game {
     }
 }
 
+enum CustomEvent {
+    RunGameLogic,
+}
+
+struct Terminate;
+
 impl Game {
     pub fn new() -> Self {
         Default::default()
@@ -43,7 +58,7 @@ impl Game {
 
     pub fn add_once(
         mut self,
-        once: impl FnMut(&mut Commands, &mut AnyMap, &mut EventHandle) + 'static,
+        once: impl FnMut(&mut Commands, &mut SendAnyMap, &mut EventHandle) + 'static + Send,
     ) -> Self {
         self.scheduler.add_once(Box::new(once));
         self
@@ -51,7 +66,7 @@ impl Game {
 
     pub fn add_update(
         mut self,
-        update: impl FnMut(&mut Commands, &mut AnyMap, &mut EventHandle) + 'static,
+        update: impl FnMut(&mut Commands, &mut SendAnyMap, &mut EventHandle) + 'static + Send,
     ) -> Self {
         self.scheduler.add_update(Box::new(update));
         self
@@ -60,28 +75,225 @@ impl Game {
     #[inline]
     pub fn prepare(&mut self) {}
 
-    fn exit(&mut self, event_loop: ActiveEventLoop) {}
+    //fn exit(&mut self, event_loop: ActiveEventLoop) {}
 
-    #[inline]
     pub fn run(mut self) {
-        env_logger::init();
         warn!("Starting event loop");
         //TODO!: nicer error handling -> (inline) function
         let event_loop = EventLoop::with_user_event().build();
-        if let Err(e) = event_loop {
-            let message = format!("Fatal error while building the event loop: {}", e);
-            error!("{}", message);
-            panic!("{}", message);
+
+        match event_loop {
+            Err(e) => {
+                let message = format!("Fatal error while building the event loop: {}", e);
+                error!("{}", message);
+                panic!("{}", message);
+            }
+            Ok(event_loop) => {
+                let proxy = event_loop.create_proxy();
+
+                let (tx, rx) = std::sync::mpsc::sync_channel::<Terminate>(0);
+
+                self.termination_tx = Some(tx);
+
+                //clock
+                std::thread::spawn(move || {
+                    while rx.try_recv().is_err() {
+                        let then = Instant::now();
+                        if let Err(_) = proxy.send_event(CustomEvent::RunGameLogic) {
+                            error!("Event loop doesn't exist anymore, terminating the clock");
+                            break;
+                        };
+
+                        let time_to_sleep =
+                            Instant::from(then + Duration::from_secs_f64(1. / FPS as f64))
+                                .saturating_duration_since(Instant::now());
+
+                        match time_to_sleep {
+                            duration => std::thread::sleep(duration),
+                            Duration::ZERO => {}
+                        }
+                    }
+                    warn!("Clock terminating");
+                    //optional termination logic
+                });
+
+                warn!("Preps finished");
+
+                //let rwlock=RwLock::new(self);
+                if let Err(e) = event_loop.run_app(&mut self) {
+                    let message = format!("Fatal error while running the event loop: {}", e);
+                    error!("{}", message);
+                    panic!("{}", message);
+                }
+            }
         }
-        println!("Preparations finished");
-        if let Err(e) = event_loop
-            .expect("Would have panicked if there was an error with the event loop")
-            .run_app(&mut self)
-        {
-            let message = format!("Fatal error while running the event loop: {}", e);
-            error!("{}", message);
-            panic!("{}", message);
+    }
+}
+
+impl ApplicationHandler<CustomEvent> for Game {
+    //TODO!: make sure initialisation only happens once, resume() doesn't guarantee that!!
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        if self.graphics_state.is_none() {
+            event_loop.set_control_flow(ControlFlow::Poll);
+
+            let window = Arc::new(
+                event_loop
+                    .create_window(self.window_attributes.clone())
+                    .expect(
+                        "Winit needs to be able to create a window, otherwise everything won't work",
+                    ),
+            );
+
+            self.graphics_state = Some(
+                pollster::block_on(State::new(
+                    window,
+                    self.graphics_state_config
+                        .take()
+                        .expect("Set to Some(_) before running the game"),
+                ))
+                //TODO: proper exception handling
+                .expect("State creation needs to work"),
+            );
         }
+
+        //once logic
+
+        self.scheduler
+            .setup(&mut self.commands, &mut self.resources, &mut self.events);
+
+        self.events.update();
+
+        CommandHandle::update(self);
+    }
+    fn user_event(&mut self, event_loop: &ActiveEventLoop, event: CustomEvent) {
+        match event {
+            CustomEvent::RunGameLogic => {}
+        }
+
+        self.scheduler
+            .update(&mut self.commands, &mut self.resources, &mut self.events);
+
+        self.events.update();
+        CommandHandle::update(self);
+    }
+    //TODO
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        event: WindowEvent,
+    ) {
+        /*if event != WindowEvent::RedrawRequested {
+            println!("{:?}", event);
+        }*/
+
+
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            //I should refactor that later - TODO
+            WindowEvent::Resized(size) => {
+                if let Some(graphics_state) = &mut self.graphics_state {
+                    graphics_state.resize(size.width, size.height)
+                };
+            }
+            WindowEvent::MouseInput {
+                state: btn_state,
+                button,
+                ..
+            } => self
+                .graphics_state
+                .as_mut()
+                .unwrap()
+                .handle_mouse_button(button, btn_state.is_pressed()),
+            WindowEvent::MouseWheel { delta, .. } => {
+                self.graphics_state
+                    .as_mut()
+                    .unwrap()
+                    .handle_mouse_scroll(&delta);
+            }
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        physical_key: PhysicalKey::Code(code),
+                        state: key_state,
+                        ..
+                    },
+                ..
+            } => self.graphics_state.as_mut().unwrap().handle_key(
+                event_loop,
+                code,
+                key_state.is_pressed(),
+            ),
+            WindowEvent::RedrawRequested => {
+                //TODO: check if this is actually necessary later
+                if self.graphics_state.is_none() {
+                    return;
+                }
+
+                let elapsed = self.last_iteration.elapsed();
+                self.last_iteration = Instant::now();
+                self.graphics_state
+                    .as_mut()
+                    .expect("GENERIC")
+                    .update(elapsed);
+
+                match self.graphics_state.as_mut().expect("fgfdsyxcfr").render(
+                    self.world.to_be_rendered.iter().filter_map(|id| {
+                        //TODO: .expect messages
+                        //TODO: more efficient borrowing instead of only .as_ref / .as_mut
+
+                        match self.world.entities.get(id).expect("").model {
+                            ModelState::Visible(model_id) => Some(model_id),
+                            _ => None,
+                        }
+                    }),
+                ) {
+                    Ok(_) => {}
+                    // Reconfigure the surface if it's lost or outdated
+                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                        let size = self.graphics_state.as_ref().expect("").window.inner_size();
+                        self.graphics_state
+                            .as_mut()
+                            .expect("")
+                            .resize(size.width, size.height);
+                    }
+                    Err(e) => {
+                        log::error!("Unable to render {}", e);
+                    }
+                }
+            }
+            event => self.propagate_event(event),
+        };
+    }
+
+    fn device_event(
+        &mut self,
+        _event_loop: &ActiveEventLoop,
+        _device_id: DeviceId,
+        event: DeviceEvent,
+    ) {
+        let state = if let Some(state) = &mut self.graphics_state {
+            state
+        } else {
+            return;
+        };
+        match event {
+            DeviceEvent::MouseMotion { delta: (dx, dy) } => {
+                if state.mouse_pressed {
+                    state.camera_controller.handle_mouse(dx, dy);
+                }
+            }
+            _ => {}
+        }
+    }
+
+    fn exiting(&mut self, event_loop: &ActiveEventLoop) {
+        //the only possible error is a terminated clock and that's not a bad thing since we're terminating now
+        let _ = self
+            .termination_tx
+            .as_ref()
+            .expect("Always Some(_) while (and after) running the loop")
+            .send(Terminate);
     }
 }
 
@@ -128,17 +340,21 @@ impl Game {
 //internal helper functions
 impl Game {
     #[inline]
-    fn propagate_event<T: 'static>(&mut self, event: T) {
+    fn propagate_event<T: 'static + Send>(&mut self, event: T) {
         self.events.add(event);
     }
 }
 
 use log::{error, warn};
-use std::sync::Arc;
+use std::sync::{Arc, Mutex, RwLock, atomic};
+use std::thread::JoinHandle;
+use std::time::{Duration, Instant};
 //use crate::custom_events::input::{SimpleKeyEvent, SimpleMouseKeyEvent};
-use crate::aliases::Commands;
+use crate::aliases::{Commands, SendAnyMap};
+use crate::commands::CommandHandle;
 use crate::world::{ModelState, World};
 use graphics_core::config::{Asset, StateConfig};
+use winit::event_loop::{ControlFlow, EventLoopProxy};
 use winit::window::WindowAttributes;
 use winit::{
     application::ApplicationHandler,
@@ -147,99 +363,3 @@ use winit::{
     keyboard::{KeyCode, PhysicalKey},
     window::Window,
 };
-
-impl ApplicationHandler<Game> for Game {
-    /*fn new_events(&mut self, event_loop: &ActiveEventLoop, cause: StartCause) {
-        todo!()
-    }*/
-
-    //TODO!: make sure initialisation only happens once, resume() doesn't guarantee that!!
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        let window = Arc::new(
-            event_loop
-                .create_window(self.window_attributes.clone())
-                .expect(
-                    "Winit needs to be able to create a window, otherwise everything won't work",
-                ),
-        );
-
-        self.graphics_state = Some(
-            pollster::block_on(State::new(
-                window,
-                self.graphics_state_config
-                    .take()
-                    .expect("Set to Some(_) before running the game"),
-            ))
-            //TODO: proper exception handling
-            .expect("State creation needs to work"),
-        );
-        //once logic
-
-        self.scheduler
-            .setup(&mut self.commands, &mut self.resources, &mut self.events);
-
-        self.events.update();
-    }
-
-    fn user_event(&mut self, _event_loop: &ActiveEventLoop, event: Game) {
-        *self = event;
-    }
-    fn window_event(
-        &mut self,
-        event_loop: &ActiveEventLoop,
-        _window_id: winit::window::WindowId,
-        event: WindowEvent,
-    ) {
-        self.scheduler
-            .update(&mut self.commands, &mut self.resources, &mut self.events);
-        self.events.update();
-
-        match event {
-            WindowEvent::CloseRequested => event_loop.exit(),
-            //I should refactor that later - TODO
-            WindowEvent::Resized(size) => {
-                if let Some(graphics_state) = &mut self.graphics_state {
-                    graphics_state.resize(size.width, size.height)
-                };
-            }
-            WindowEvent::RedrawRequested => {
-                //TODO: check if this is actually necessary later
-                if self.graphics_state.is_none() {
-                    return;
-                }
-
-                self.graphics_state.as_mut().expect("GENERIC").update();
-
-                match self.graphics_state.as_mut().expect("fgfdsyxcfr").render(
-                    self.world.to_be_rendered.iter().filter_map(|id| {
-                        //TODO: .expect messages
-                        //TODO: more efficient borrowing instead of only .as_ref / .as_mut
-                        match self.world.entities.get(id).expect("").model {
-                            ModelState::Visible(model_id) => Some(model_id),
-                            _ => None,
-                        }
-                    }),
-                ) {
-                    Ok(_) => {}
-                    // Reconfigure the surface if it's lost or outdated
-                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
-                        let size = self.graphics_state.as_ref().expect("").window.inner_size();
-                        self.graphics_state
-                            .as_mut()
-                            .expect("")
-                            .resize(size.width, size.height);
-                    }
-                    Err(e) => {
-                        log::error!("Unable to render {}", e);
-                    }
-                }
-            }
-            event => self.propagate_event(event),
-        };
-    }
-    fn memory_warning(&mut self, event_loop: &ActiveEventLoop) {
-        event_loop.exit();
-        panic!("Received a memory warning, currently a fatal error");
-        todo!();
-    }
-}
