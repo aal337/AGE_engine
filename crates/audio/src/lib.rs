f38807e5283426c7c6b6b09f025eca489b801708
HQ2000-CPP <194770151+HQ2000-CPP@users.noreply.github.com>
2025-12-31 14:05:36 +0100

    renaming (sorry for the diff, it's not really important anyway

--- crates/audio/src/lib.rs
+++
@@ -1,6 +0,0 @@
-//TODO: make audio ECS-ready
-
-//maybe not everything needs to be pub in the end, idk
-pub mod errors;
-pub mod output_handle;
-pub mod traits;
--- crates/audio/src/traits.rs
+++
@@ -1,8 +0,0 @@
-//maybe I will remove them idk
-pub mod marker {
-    use crate::output_handle::output_markers;
-
-    pub trait OutputHandlerState {}
-    impl OutputHandlerState for output_markers::OutputDisabled {}
-    impl OutputHandlerState for output_markers::OutputEnabled {}
-}
